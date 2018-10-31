1967cd3a3c24e9dbbd5a7b8039ff4f68c9a6f4c1
BoHongtao <BoHongtao@yeah.net>
2018-10-31 15:18:23 +0800

    add save

--- spider-test.js
+++ spider-test.js
@@ -5,10 +5,17 @@
 //引入需要的包
 var request = require('request');
 var cheerio = require('cheerio');
-
+var mysql   = require('mysql');
+var connection = mysql.createConnection({
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '',
+    database : 'spider'
+});
 var url = "https://jn.zu.anjuke.com/fangyuan/p2/"
-
-//请求数据
+/*
+ * 请求数据
+ */
 function dataRequest(dataurl) {
     request({
         url:dataurl,
@@ -27,6 +34,24 @@ function dataRequest(dataurl) {
 }
 
 /*
+ * 保存到mysql
+ */
+function save(title,info,add) {
+    var  addSql = 'INSERT INTO node_room(id,title,info,address) VALUES(0,?,?,?)';
+    var  addSqlParams = [title,info, add];
+    connection.query(addSql,addSqlParams,function (err, result) {
+        if(err){
+            console.log(err.sql)
+            console.log('[INSERT ERROR] - ',err.message);
+            return;
+        }
+        console.log('--------------------------INSERT----------------------------');
+        console.log('保存数据',addSqlParams);
+        console.log('-----------------------------------------------------------------\n\n');
+    });
+}
+
+/*
  * 解析HTML
  */
 function dataPraseDolphin(html) {
@@ -36,15 +61,9 @@ function dataPraseDolphin(html) {
         var room = rooms[i];
         var title = $(room).find('.zu-info').find('h3').text().replace(/(^\s*)|(\s*$)/g, "");
         var info = $(room).find('.zu-info').find('.tag').text().replace(/(^\s*)|(\s*$)/g, "");
-        var add = $(room).find('.zu-info').find('address').text().replace(/(^\s*)|(\s*$)/g, "").replace('\n','').replace(' ','');
-
-        console.log(title);
-        console.log(info);
-        console.log(add);
+        var add = $(room).find('.zu-info').find('address').text().replace(/(^\s*)|(\s*$)/g, "").replace('\n','').replace(' ','').replace(/\s/g, "");
+        save(title,info,add);
     }
-    // console.log(rooms)
-
 }
-
-
-dataRequest(url)
+//发起请求
+dataRequest(url);
